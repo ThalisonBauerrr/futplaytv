57d39dea90ce1934b3e44522d998a9287cd68e68
Seu Nome <seuemail@dominio.com>
2025-04-30 21:31:02 -0300

    alter name db

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const db = mysql.createPool({
   host: '127.0.0.1',  // IP do seu servidor MySQL
   user: 'bauerthalison',    // Seu usuário MySQL
   password: 'p5pexvm',      // Sua senha do MySQL
-  database: 'futplay',      // Nome do banco de dados
+  database: 'futplaytv',      // Nome do banco de dados
   waitForConnections: true, // Espera por conexões disponíveis
   connectionLimit: 10,      // Limite de conexões no pool
   queueLimit: 0             // Limite de espera na fila
